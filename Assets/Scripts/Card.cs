145793ef5348648d947bd0047c38e9de2d677f21
SaraNagy <sara.nagy221@gmail.com>
2024-10-16 15:19:24 +0300

    Card Animations

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Animator Anim;
+    Sprite CardBGSprite;
+    [SerializeField] Sprite CardSprite;
+
     void Start()
     {
-        
+        Anim = GetComponent<Animator>();
+        CardBGSprite = GetComponent<Image>().sprite;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeSprite() 
     {
-        
+        GetComponent<Image>().sprite = CardSprite;
+        Anim.SetBool("Flip", true);
+    }
+    public void ReturnSprite()
+    {
+        GetComponent<Image>().sprite = CardBGSprite;
+        Anim.SetBool("Return", true);
+    }
+    public void Return()
+    {
+        Anim.SetBool("Flip", false);
+        Anim.SetBool("Return", false);
+
+    }
+    public void OnClickCard()
+    {
+        if (Anim.GetBool("Flip")) return;
+
+        Anim.SetBool("Flip",true);
     }
 }
